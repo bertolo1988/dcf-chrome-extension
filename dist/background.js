0324b587a84fde15535ba4adcb793529992cbc02
Tiago <tiagobertolo@gmail.com>
2021-03-18 00:50:21 +0000

    Add dist folder.

---
+++ dist/background.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!***************************!*\
+  !*** ./src/background.js ***!
+  \***************************/
+console.log('nop');
+
+/******/ })()
+;
+//# sourceMappingURL=background.js.map
